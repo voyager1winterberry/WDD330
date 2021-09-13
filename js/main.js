26fb5b15b11cdfab1cb3f263fd60255c0ece62c9
Marcus <voyager1winterberry@gmail.com>
2021-09-13 16:51:02 -0600

    fixed problem with portfolio, adding week 1 content

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// links to different weeks of assignments
 const links = [
     {
         label: "Week 1",
@@ -10,18 +9,17 @@ const links = [
     }
 ]
 
-// populates an unordered list with the links in the links array
+// populates an ordered list with the links array
 function createList() {
     for (let i = 0; i < links.length; i++) {
         // for each link, create <li> tag to put in <ol>
-        // wrap <a> tag inside <li> to access href attribute, which allows a link
+        // wrap <a> tag inside <li> to use href attribute for link
         const newA = document.createElement("a");
         const newLi = document.createElement("li");
         const newLabel = document.createTextNode(links[i].label);
-        const newUrl = document.createTextNode(links[i].url);
+        const newUrl = links[i].url;
         const newOl = document.getElementById("portfolioItems");
 
-        // .innerHTML = '<a href="' + newURL + '"></a>'
         newA.appendChild(newLabel);
         newA.setAttribute("href", newUrl);        
         newLi.appendChild(newA);
