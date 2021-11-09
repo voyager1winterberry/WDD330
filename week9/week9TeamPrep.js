797e6341f948f6f3e722dc029d445c97fcbc386a
Marcus <voyager1winterberry@gmail.com>
2021-11-08 19:28:46 -0700

    team assignment

---
+++ week9/week9TeamPrep.js
@@ -0,0 +1,24 @@
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  // console.log(e.propertyName);
+  // console.log(this)
+  this.classList.remove('playing');
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+console.log(keys)
+keys.forEach((key) => {
+  key.addEventListener('transitionend', removeTransition);
+});
+
+window.addEventListener('keydown', playSound);
